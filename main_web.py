02f3b7d0932c69ecc5f9cf73f6cff18df999b724
Cristian <c.perezc2018@gmail.com>
2022-04-07 09:05:26 +0200

    Showing codes

--- main_web.py
+++ main_web.py
@@ -7,6 +7,14 @@ def home():
     return render_template("index.html")
 @app.route('/null')
 def null():
-    return render_template("null.html")
+    s = []
+    with open("codes/Sorting.py") as f:
+        lines = f.readlines()
+        i = 0
+        while lines[i] != "#Mergesort\n":
+            i+=1
+        for j in range(i + 1, len(lines)):
+            s.append(lines[j])
+    return render_template("null.html", code_list = s)
 if __name__ == '__main__':
     app.run(debug=True)
